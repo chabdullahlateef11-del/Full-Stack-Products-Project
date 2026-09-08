8127224409ddeddb97fe45a46a7842b230673ad9
abdullah <chabdullahlateef11@gmail.com>
2026-09-08 08:40:04 +0500

    Add 0.0.0.0 binding for Railway

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get(/^(?!\/api).*/, (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
